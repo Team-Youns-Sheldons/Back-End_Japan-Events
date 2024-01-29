9477908b03d468111a14c88da7f2e6841ed0760f
Juan Munguia <carlosmunguia23145@gmail.com>
2024-01-29 15:01:28 +0100

    create and configurated repository2

---
+++ src/main/java/com/gulag/JapanEvents/JapanEventsApplication.java
@@ -0,0 +1,13 @@
+package com.gulag.JapanEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanEventsApplication.class, args);
+	}
+
+}
